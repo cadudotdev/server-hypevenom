9dfd2ee9b32b7d14bdf01b86923eed2f2c2044dd
carlos.santos <carlos.santos@neomind.com.br>
2024-01-29 04:45:12 -0300

    refac import modules

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ArtistModule } from './artist/artist.module';
-import { ArtistInfoModule } from './artistInfo/artistInfo.module';
-import { TrackModule } from './track/track.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    TrackModule,
-    ArtistModule,
-    ArtistInfoModule,
-    UserModule,
-    AuthModule,
-  ],
+  imports: [AuthModule],
   controllers: [],
   providers: [],
 })
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
-import { AuthController } from './auth.controller';
+import { TrackModule } from '../track/track.module';
+import { ArtistModule } from '../artist/artist.module';
+import { ArtistInfoModule } from '../artistInfo/artistInfo.module';
 
 @Module({
   imports: [
     UserModule,
+    TrackModule,
+    ArtistModule,
+    ArtistInfoModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
