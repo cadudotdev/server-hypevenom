04154e53f37bf06d22242bec77bb979d53a9844b
carlos.santos <carlos.santos@neomind.com.br>
2024-02-10 00:32:27 -0300

    feat: dockerização e variáveis de ambiente

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { config } from 'dotenv';
 import { AppModule } from './module/app.module';
 import { handleCors } from './utils/cors';
 
-config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   handleCors(app);
--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 
 export function handleCors(app: INestApplication<any>) {
-  process.env.DEVELOPMENT &&
+  process.env.NODE_ENV === 'development' &&
     app.enableCors({
       origin: `http://localhost:${process.env.CLIENT_DEVELOPMENT_PORT}`,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
